af7d83a3599ec42e535b41e7863474971058e7e2
purna0123 <119773088+purna0123@users.noreply.github.com>
2022-12-04 14:02:56 +0530

    Add files via upload

---
+++ src/test/java/TestScenarios/LoginTest.java
@@ -0,0 +1,27 @@
+package TestScenarios;
+
+import OrangePages.OrangeHomePage;
+import OrangePages.OrangeLoginPage;
+import browserSetup.BrowserSetup;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BrowserSetup {
+
+    OrangeHomePage objOrangeHomePage;
+    OrangeLoginPage objOrangeLoginPage;
+
+
+
+    String userName="Admin";
+    String password="admin123";
+
+    @Test(priority = 1)
+    public void loginWithValidCredentials() {
+        objOrangeLoginPage = new OrangeLoginPage(driver);
+        objOrangeLoginPage.veryHeader();
+        objOrangeLoginPage.loginOrange(userName, password);
+        System.out.println(userName);
+        System.out.println(password);
+    }
+
+}
